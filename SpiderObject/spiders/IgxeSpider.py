1cd4af02d672cd752c76534d7d2ae040054adc92
IReallyLovePython <542686015@qq.com>
2018-04-17 12:25:27 +0800

    模拟登陆

--- SpiderObject/spiders/IgxeSpider.py
+++ SpiderObject/spiders/IgxeSpider.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy import Selector, FormRequest
+
 from SpiderObject.items import SpiderobjectItem
 
 
@@ -8,13 +10,29 @@ class IgxespiderSpider(scrapy.Spider):
     allowed_domains = ['igxe.cn']
 
     page = 1
-    url = 'https://www.igxe.cn/pubg/578080/%s/0/0' % page
+    url = 'https://www.igxe.cn/login'
 
     start_urls = [
         url
     ]
+    headers = {
+        'User-Agent': 'Mozilla/5.0(Windows NT 10.0;WOW64) AppleWebKit/537.36(KHTML, likeGecko) Chrome/65.0.3325.181Safari/537.36'
+    }
 
     def parse(self, response):
+        csrf = Selector(response).xpath('//input[@name="csrfmiddlewaretoken"]/@value').extract()[0]
+        return [FormRequest.from_response(response,  # "https://www.igxe.cn/login",
+                                          headers=self.headers,  # 注意此处的headers
+                                          formdata={
+                                              'csrfmiddlewaretoken': csrf,
+                                              'username': 'h877969945j',
+                                              'password': 'h25254818j.',
+                                          },
+                                          callback=self.parse_page,
+                                          dont_filter=True
+                                          )]
+
+    def parse_page(self, response):
         for each in response.xpath("//div[@class='mod-theKeyItem']"):
             item = SpiderobjectItem()
 
@@ -33,6 +51,7 @@ class IgxespiderSpider(scrapy.Spider):
 
             yield item
 
-        # if self.page < 12:
-        #     self.page += 1
-        # yield scrapy.Request(self.url, callback=self.parse)
+        url = 'https://www.igxe.cn/pubg/578080/%s/0/0' % self.page
+        if self.page <12:
+            self.page += 1
+        yield scrapy.Request(url, callback=self.parse_page)
